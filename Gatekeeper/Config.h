cda18844b42e6a99a35d394437bbf268d48ff63b
dpslwk <dps.lwk@gmail.com>
2016-12-03 12:46:29 +0000

    Gatekeeper: fix door bell

--- Gatekeeper/Config.h
+++ Gatekeeper/Config.h
@@ -55,7 +55,7 @@ byte ip[]     = { 192, 168, 0, 10 };
 // timeout in mills for how often the doorbell can be rang
 #define DOOR_BUTTON_TIMEOUT 5000
 unsigned long doorTimeOut = 0;
-boolean doorButtonState = 0;
+uint8_t doorButtonState = 0;
 
 // Status Indicator's 
 // BLUE = UNLOCKED
